8b92ce54aebe2b45e616338876958b7f5609599b
utaware <1264051408@qq.com>
2022-07-04 22:46:55 +0800

    chore: modify github workflows branch name

--- script/test.js
+++
@@ -1,8 +0,0 @@
-import path from 'path'
-
-const path1 = 'D:\\utaware\\docs\\web\\browser\\chrome\\CR-01.md'
-const path2 = 'D:\\utaware\\docs'
-
-const r = path.relative(path2, path1).split(path.sep).join('/')
-
-console.log(r)
